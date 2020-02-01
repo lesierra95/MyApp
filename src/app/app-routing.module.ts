4bdd254b02b9ae62eb985d8d177764eaf38c5d45
lherrera <luis.herrera@cyxtera.com>
2020-02-01 12:05:30 -0500

    organize the code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { FilmsComponent } from './films/films.component';
-import { PeopleComponent } from './people/people.component';
-import { SpeciesComponent } from './species/species.component';
-import { PlanetsComponent } from './planents/planets.component';
-import { VehiclesComponent } from './vehicles/vehicles.component';
-import { StarshipsComponent } from './starships/starships.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {FilmsComponent} from './films/films.component';
+import {PeopleComponent} from './people/people.component';
+import {SpeciesComponent} from './species/species.component';
+import {PlanetsComponent} from './planets/planets.component';
+import {VehiclesComponent} from './vehicles/vehicles.component';
+import {StarshipsComponent} from './starships/starships.component';
 
 
 const routes: Routes = [
-  {path: 'films', component: FilmsComponent },
+  {path: 'films', component: FilmsComponent},
   {path: 'people', component: PeopleComponent},
-  {path: 'planets', component: PlanetsComponent },
+  {path: 'planets', component: PlanetsComponent},
   {path: 'species', component: SpeciesComponent},
   {path: 'vehicles', component: VehiclesComponent},
   {path: 'starships', component: StarshipsComponent},
-  { path: '**', component: FilmsComponent }
+  {path: '**', component: FilmsComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FilmsComponent } from './films/films.component';
-import { PeopleComponent } from './people/people.component';
-import { StarshipsComponent } from './starships/starships.component';
-import { VehiclesComponent } from './vehicles/vehicles.component';
-import { SpeciesComponent } from './species/species.component';
-import { PlanetsComponent } from './planents/planets.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FilmsComponent} from './films/films.component';
+import {PeopleComponent} from './people/people.component';
+import {StarshipsComponent} from './starships/starships.component';
+import {VehiclesComponent} from './vehicles/vehicles.component';
+import {SpeciesComponent} from './species/species.component';
+import {PlanetsComponent} from './planets/planets.component';
 
 @NgModule({
   declarations: [
@@ -30,4 +29,5 @@ import { PlanetsComponent } from './planents/planets.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
